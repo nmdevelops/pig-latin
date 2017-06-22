aacb6fbb6c20df69498c8cb4661600791c03c58d
Kirsten Knapp and Nathan Mayer <kirsten@>
2017-06-21 19:57:34 -0700

    add behavior 2, words beginning with vowels add way to end

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,39 @@
-//back-end
+//to be moved to back end
 var string;
+//to be moved to front end
+string = prompt("Phrase to translate?");
+//back-end
+var translatedString;
+//console.log(string);
 //behavior 2
+var pigTranslate = (function () {
+
+  string = string.toLowerCase();
+  //console.log(string);
+  // var vowelsRE = new RegExp('a*e*i*o*u');
+// rabbit hole....    var vowelsRE = /'a*e*i*o*u'/;
+
+
+  if (string.charAt(0) === "a" || string.charAt(0) === "e" || string.charAt(0) === "i" || string.charAt(0) === "o" || string.charAt(0) === "u" ) {
+    translatedString = string + "way";
+    alert(translatedString);
+
+    // behavior 3
+  }
+
+
+
+})
+
+
+
 
 
 
 
 
 //front-end
-string = prompt("Phrase to translate?")
+$(document).ready(function () {
+
+  pigTranslate();
+})

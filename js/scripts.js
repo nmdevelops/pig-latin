759b6cddb5ca74f37a8950e06fb8cba9efa4621e
Kirsten Knapp and Nathan Mayer <kirsten@>
2017-06-21 20:56:46 -0700

    add behavior 4, words beginning with a consonant string, slice consonant string off and append to remainder add ay to end of string

--- js/scripts.js
+++ js/scripts.js
@@ -5,6 +5,8 @@ var string;
 string = prompt("Phrase to translate?");
 //back-end
 var translatedString;
+var stopLoop=10;
+var sliceLimit = 0;
 //console.log(string);
 //behavior 2
 var pigTranslate = (function () {
@@ -13,16 +15,31 @@ var pigTranslate = (function () {
   //console.log(string);
   // var vowelsRE = new RegExp('a*e*i*o*u');
 // rabbit hole....    var vowelsRE = /'a*e*i*o*u'/;
+//behavior 4 insertion
+for (var i = 0; i < stopLoop; i++) {
 
+  if (string.charAt(i) === "a" || string.charAt(i) === "e" || string.charAt(i) === "i" || string.charAt(i) === "o" || string.charAt(i) === "u" ) {
 
-  if (string.charAt(0) === "a" || string.charAt(0) === "e" || string.charAt(0) === "i" || string.charAt(0) === "o" || string.charAt(0) === "u" ) {
+    stopLoop = i;
+
+  } else {
+  sliceLimit +=1;
+  }
+}
+//behavior 4 insertion
+
+  // if (string.charAt(0) === "a" || string.charAt(0) === "e" || string.charAt(0) === "i" || string.charAt(0) === "o" || string.charAt(0) === "u" ) {
+  //   translatedString = string + "way";
+  //   alert(translatedString);
+  // }
+  if (sliceLimit === 0) {
     translatedString = string + "way";
     alert(translatedString);
   }
 // behavior 3
 else {
- var consonantString = string.slice(0,1);
- var stringBase = string.slice(1);
+ var consonantString = string.slice(0,sliceLimit);
+ var stringBase = string.slice(sliceLimit);
  alert(stringBase + consonantString + "ay");
 // alert("whatsup")
 }
